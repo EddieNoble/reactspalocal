0b8f4eb42b0ab9d532d74461354a70bc7d67223d
enoble <enoble@cascatacom.co.uk>
2024-05-22 13:17:15 +0100

    Initial Commit

---
+++ src/AuthData.js
@@ -0,0 +1,33 @@
+function AuthData()
+{
+    return (
+<div>
+    <header><h2>Auth Data</h2></header>
+<table class="table">
+<th>
+    <td>Key</td>
+    <td>Value</td>
+</th>
+<tr>
+    <td>Display name</td>
+    <td>identity-client-spa</td>
+</tr>
+<tr>
+    <td>Application (client) ID</td>
+    <td>f5707ce0-5364-449f-9704-9b8662144923</td>
+</tr>
+<tr>
+    <td>Object ID</td>
+    <td>75d78048-6ac8-4a01-8ed4-80f82b346e73</td>
+</tr>
+<tr>
+    <td>Directory (tenant) ID</td>
+    <td>803558d7-1200-49d0-ae73-40729ef12e8a</td>
+</tr>
+</table>
+
+</div>
+    );
+}
+
+export default AuthData;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import App from './'
+import AuthData from './AuthData';
 import reportWebVitals from './reportWebVitals';
 
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+// Bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+      <MsalProvider instance={msalInstance}>
+          <App />
+      </MsalProvider>
   </React.StrictMode>
 );
 
